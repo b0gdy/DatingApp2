16b3ce729a9677237ba0472aff004d0ef1474e12
Bogdan Badea <badeabogdanandrei98@yahoo.com>
2026-06-03 22:39:01 +0300

    End of section 3

---
+++ client/src/app/app.ts
@@ -0,0 +1,39 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { lastValueFrom } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  imports: [],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App implements OnInit {
+  private http = inject(HttpClient);
+  protected title = 'Dating app';
+  protected members = signal<any>([])
+
+  // ngOnInit(): void {
+  //   this.http.get('https://localhost:5001/api/members').subscribe({
+  //     next: response => this.members.set(response),
+  //     error: error => console.error(error),
+  //     complete: () => console.log('Completed the http request')
+  //   })
+  // }
+
+  async ngOnInit() {
+    this.members.set(await this.getMembers())
+  }
+
+  async getMembers() {
+    try
+    {
+      return lastValueFrom(this.http.get('https://localhost:5001/api/members'));
+    }
+    catch (error)
+    {
+      console.log(error);
+      throw error;
+    }
+  }
+}
