474711815b5ba3f09bbdfcc1aac51c3a31bf9ee7
Bogdan Badea <badeabogdanandrei98@yahoo.com>
2026-06-10 22:25:31 +0300

    End of section 8

---
+++ API/Data/Migrations/20260610190736_FixDbContext.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixDbContext : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Member_Users_Id",
+                table: "Member");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Photos_Member_MemberId",
+                table: "Photos");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Member",
+                table: "Member");
+
+            migrationBuilder.RenameTable(
+                name: "Member",
+                newName: "Members");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Members",
+                table: "Members",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Members_Users_Id",
+                table: "Members",
+                column: "Id",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Photos_Members_MemberId",
+                table: "Photos",
+                column: "MemberId",
+                principalTable: "Members",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Members_Users_Id",
+                table: "Members");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Photos_Members_MemberId",
+                table: "Photos");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Members",
+                table: "Members");
+
+            migrationBuilder.RenameTable(
+                name: "Members",
+                newName: "Member");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Member",
+                table: "Member",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Member_Users_Id",
+                table: "Member",
+                column: "Id",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Photos_Member_MemberId",
+                table: "Photos",
+                column: "MemberId",
+                principalTable: "Member",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
